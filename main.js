ede86e1807db59b7bcfde636dd31727190572576
aigerimkurmasheva <111555309+aigerimkurmasheva@users.noreply.github.com>
2022-10-24 16:33:03 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+
+function perimeter(length, width) {
+result=2*length+2*width
+return result
+}
+
+let length=2
+let width=3
+let p=2*length+2*width
+console.log(p)
+
+function convert(dollar) {
+result=dollar*469
+return result
+}
+
+let dollar=50
+let c=dollar*469
+console.log(c)
+
+let x=2
+if (x>0) {
+console.log("x is positive")
+}
+
+else {
+console.log("x is negative")
+}
